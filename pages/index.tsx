e312d2d2e9018b80e46968db1fdcd0ea5089b8db
Ed Yu <edlyu@hotmail.com>
2023-04-25 11:27:21 -0700

    Add logout functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,34 @@
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
+
   return (
-    <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+    <>
+      <h1 className="text-4xl text-green-500">Netflix Clone</h1>
+      <p className="text-white">Logged in as : {user?.email}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout</button>
+    </>
   )
 }
