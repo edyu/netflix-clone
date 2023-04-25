eb99c17283e4be7775b2d3e5d047bdff9f8ee4e6
Ed Yu <edlyu@hotmail.com>
2023-04-25 16:53:52 -0700

    Add profile

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,14 +2,11 @@ import { useCallback, useState } from "react";
 import axios from "axios";
 import Input from "@/components/input";
 import { signIn } from 'next-auth/react';
-import { useRouter } from 'next/router';
 
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 
 const Auth = () => {
-    const router = useRouter();
-
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
@@ -25,15 +22,12 @@ const Auth = () => {
             await signIn('credentials', {
                 email,
                 password,
-                redirect: false,
-                callbackUrl: '/',
+                callbackUrl: '/profiles',
             });
-
-            router.push('/');
         } catch (error) {
             console.log(error);
         }
-    }, [email, password, router]);
+    }, [email, password]);
 
     const register = useCallback(async () => {
         try {
@@ -90,12 +84,12 @@ const Auth = () => {
                         </button>
                         <div className="flex flex-row items-center gap-4 mt-8 justify-center">
                             <div
-                                onClick={ () => signIn('google', { callbackUrl: '/'})}
+                                onClick={ () => signIn('google', { callbackUrl: '/profiles'})}
                                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                                 <FcGoogle size={30} />
                             </div>
                             <div
-                                onClick={ () => signIn('github', { callbackUrl: '/' }) }
+                                onClick={ () => signIn('github', { callbackUrl: '/profiles' }) }
                                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                                 <FaGithub size={30} />
                             </div>
