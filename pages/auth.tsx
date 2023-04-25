4032f28cca9e4b127a141867a6ef98220edf65da
Ed Yu <edlyu@hotmail.com>
2023-04-24 17:45:55 -0700

    Add register and login

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,7 @@
 import { useCallback, useState } from "react";
-import Input from "../components/input";
+import axios from "axios";
+import Input from "@/components/input";
+import { signIn } from 'next-auth/react';
 
 const Auth = () => {
     const [email, setEmail] = useState('');
@@ -11,6 +13,32 @@ const Auth = () => {
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
     }, [])
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('/api/register', {
+                email,
+                name,
+                password,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+
+    }, [email, name, password]);
+
+    const login = useCallback(async () => {
+        try {
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/',
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }, [email, password]);
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -47,7 +75,7 @@ const Auth = () => {
                                 value={password}
                             />
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                             {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
                         <p className="text-neutral-500 mt-12">
