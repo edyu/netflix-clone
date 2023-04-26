47fe38b0c3c17d3b16f22f3da054c9b587087bd4
Ed Yu <ed@beachglasslabs.org>
2023-04-25 23:52:44 -0700

    Add favorite button

--- components/moviecard.tsx
+++ components/moviecard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { BsFillPlayFill } from "react-icons/bs";
+import FavoriteButton from "./favoritebutton";
 
 interface MovieCardProps {
     data: Record<string, any>;
@@ -19,6 +20,7 @@ const MovieCard: React.FC<MovieCardProps> = ({
                         <div className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300" onClick={() => {}}>
                             <BsFillPlayFill size={30} />
                         </div>
+                        <FavoriteButton movieId={data?.id} />
                     </div>
 
                     <p className="text-green-400 font-semibold mt-4">
